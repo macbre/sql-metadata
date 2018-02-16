c07a2b2bfe40f14b5de12edb501198bdcc6ffdac
macbre <maciej.brencz@gmail.com>
2018-02-16 18:38:50 +0100

    Fix pkg_resources.ContextualVersionConflict in index-digest
    pkg_resources.ContextualVersionConflict: (pytest 3.4.0 (/home/macbre/src/git/index-digest/env3/lib/python3.5/site-packages), Requirement.parse('pytest==3.2.3'), {'sql-metadata'})

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '1.1.0'
+VERSION = '1.1.1'
 
 # @see https://github.com/pypa/sampleproject/blob/master/setup.py
 setup(
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         'coverage==4.4.2',
         'pylint==1.8.1',
-        'pytest==3.2.3',
+        'pytest>=3.2.3',
         'sqlparse==0.2.4',
     ]
 )
