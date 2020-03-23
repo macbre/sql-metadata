85fbf047c914bb1ddda47352d49602f885fc3281
macbre <maciej.brencz@gmail.com>
2020-03-23 23:08:16 +0100

    v1.6.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '1.5.0'
+VERSION = '1.6.0'
 
 # @see https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
 with open("README.md", "r") as fh:
