67b687e3ff8af250c497915f485a645269c6928b
macbre <maciej.brencz@gmail.com>
2020-09-23 19:05:23 +0200

    Install black code formatter

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     python_requires='>=3.6',
     extras_require={
         'dev': [
+            'black==20.8b1',
             'coverage==5.3',
             'pylint==2.6.0',
             'pytest==6.0.2',
