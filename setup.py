c470b4efd2b06840d41bf88dffb04007bb9e13b0
Maciej Brencz <maciej.brencz@gmail.com>
2020-10-08 10:42:06 +0200

    v1.9.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '1.9.0'
+VERSION = '1.9.1'
 
 # @see https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
 with open("README.md", "r") as fh:
