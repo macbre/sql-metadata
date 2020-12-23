be0daaaf312608913e342543f6410121d6ce43e0
Maciej Brencz <maciej.brencz@gmail.com>
2020-12-23 23:45:20 +0100

    Bump to v1.10.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = "1.9.1"
+VERSION = "1.10.0"
 
 # @see https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
 with open("README.md", "r") as fh:
