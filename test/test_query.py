c3e49c2f88716589988a636d054ff5d86b2db358
macbre <maciej.brencz@gmail.com>
2020-12-23 23:15:54 +0100

    test/test_query.py: black formatter

--- test/test_query.py
+++ test/test_query.py
@@ -210,19 +210,19 @@ def test_get_query_tables():
 def test_case_insensitive():
     # case-insensitive handling
     # https://github.com/macbre/sql-metadata/issues/71
-    assert ['abc.foo', 'foo', 'bar'] == get_query_tables(
+    assert ["abc.foo", "foo", "bar"] == get_query_tables(
         "create table abc.foo as SELECT pqr.foo1 , ab.foo2 FROM foo pqr, bar ab"
     )
 
-    assert ['abc.foo', 'foo', 'bar'] == get_query_tables(
+    assert ["abc.foo", "foo", "bar"] == get_query_tables(
         "create table abc.foo as select pqr.foo1 , ab.foo2 FROM foo pqr, bar ab"
     )
 
-    assert ['pqr.foo1', 'ab.foo2'] == get_query_columns(
+    assert ["pqr.foo1", "ab.foo2"] == get_query_columns(
         "create table abc.foo as SELECT pqr.foo1 , ab.foo2 FROM foo pqr, bar ab"
     )
 
-    assert ['pqr.foo1', 'ab.foo2'] == get_query_columns(
+    assert ["pqr.foo1", "ab.foo2"] == get_query_columns(
         "create table abc.foo as select pqr.foo1 , ab.foo2 FROM foo pqr, bar ab"
     )
 
