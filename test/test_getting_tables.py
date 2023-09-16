ae5c941ff6b9102cf54bf9fdecc1c3c69140c553
Byunk <clearman001@gmail.com>
2023-09-16 15:43:09 +0900

    Add test_join_followed_by_tables

--- test/test_getting_tables.py
+++ test/test_getting_tables.py
@@ -666,3 +666,76 @@ def test_insert_into_on_duplicate_key_ipdate():
         " VALUES ('user1', 'john doe', 20)"
         " ON DUPLICATE KEY UPDATE name='john doe', age=20"
     ).tables == ["user"]
+
+
+def test_join_followed_by_tables():
+    query = """
+    SELECT 
+        web_site_id, 
+        sum(sales_price) AS sales, 
+        sum(profit) AS profit, 
+        sum(return_amt) AS returns1, 
+        sum(net_loss) AS profit_loss 
+    FROM 
+        (
+        SELECT 
+            ws_web_site_sk AS wsr_web_site_sk, 
+            ws_sold_date_sk AS date_sk, 
+            ws_ext_sales_price AS sales_price, 
+            ws_net_profit AS profit, 
+            cast(
+            0 AS decimal(7, 2)
+            ) AS return_amt, 
+            cast(
+            0 AS decimal(7, 2)
+            ) AS net_loss 
+        FROM 
+            web_sales 
+        UNION ALL 
+        SELECT 
+            ws_web_site_sk AS wsr_web_site_sk, 
+            wr_returned_date_sk AS date_sk, 
+            cast(
+            0 AS decimal(7, 2)
+            ) AS sales_price, 
+            cast(
+            0 AS decimal(7, 2)
+            ) AS profit, 
+            wr_return_amt AS return_amt, 
+            wr_net_loss AS net_loss 
+        FROM 
+            web_returns 
+            LEFT OUTER JOIN web_sales ON (
+            wr_item_sk = ws_item_sk 
+            AND wr_order_number = ws_order_number
+            )
+        ) salesreturns, 
+        date_dim, 
+        web_site 
+    WHERE 
+        date_sk = d_date_sk 
+        AND d_date BETWEEN cast('2002-08-22' AS date) 
+        AND (
+        cast('2002-08-22' AS date) + INTERVAL '14' day
+        ) 
+        AND wsr_web_site_sk = web_site_sk 
+    GROUP BY 
+        web_site_id
+    """
+    parser = Parser(query)
+    assert parser.tables == [
+        "web_sales",
+        "web_returns",
+        "date_dim",
+        "web_site"
+    ]
+
+    parser = Parser("""
+    SELECT * 
+    FROM Sales 
+        JOIN Customers 
+            ON Sales.CustomerID = Customers.CustomerID, 
+        (SELECT MAX(Revenue) FROM Sales),
+        Stores
+    """)
+    assert parser.tables == ["Sales", "Customers", "Stores"]
